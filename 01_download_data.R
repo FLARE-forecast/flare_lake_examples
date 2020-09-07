408b4f58ce78b6db7f1f5ccd502ca036715e4a48
rqthomas <rqthomas>
2020-09-07 15:12:51 -0400

    initial commit

---
+++ 01_download_data.R
@@ -0,0 +1,35 @@
+lake_directory <- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE_package/flare_fcr"
+
+config <- yaml::read_yaml(file.path(lake_directory,"observation_processing.yml"))
+
+if(!file.exists(file.path(config$data_location, config$realtime_insitu_location))){
+    stop("Missing temperature data GitHub repo")
+}
+if(!file.exists(file.path(config$data_location, config$realtime_met_station_location))){
+  stop("Missing met station data GitHub repo")
+}
+if(!file.exists(file.path(config$data_location, config$noaa_location))){
+  stop("Missing NOAA forecast GitHub repo")
+}
+if(!file.exists(file.path(config$data_location, config$manual_data_location))){
+  stop("Missing Manual data GitHub repo")
+}
+
+if(!file.exists(file.path(config$data_location, config$realtime_inflow_data_location))){
+  stop("Missing Inflow data GitHub repo")
+}
+
+setwd(file.path(config$data_location, config$realtime_insitu_location))
+system(paste0("git pull"))
+
+setwd(file.path(config$data_location, config$realtime_met_station_location))
+system(paste0("git pull"))
+
+setwd(file.path(config$data_location, config$noaa_location))
+system(paste0("git pull"))
+
+setwd(file.path(config$data_location, config$manual_data_location))
+system(paste0("git pull"))
+
+setwd(file.path(config$data_location, config$realtime_inflow_data_location))
+system(paste0("git pull"))
