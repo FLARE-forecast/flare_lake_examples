dc5aa426f71238ca1f5a387a1613c97df865b73e
rqthomas <rqthomas@vt.edu>
2020-11-24 10:11:55 -0500

    update pathing

--- fcre/04_visualize.R
+++ fcre/04_visualize.R
@@ -1,5 +1,3 @@
-#source(system.file("plotting_general.R", package="flare"))
-
 #saved_file <- "/Users/quinn/Dropbox (VTFRS)/Research/SSC_forecasting/run_flare_package/wrr_runs_obs/wrr_runs_H_2019_03_14_2019_03_15_F_16_20201124T085027.nc"
 #qaqc_data_location <- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE_package/flare_lake_examples/fcre/qaqc_data/"
 
@@ -7,7 +5,8 @@
 flare::plotting_general(file_name = saved_file,
                         qaqc_location = qaqc_data_location)
 
-source("visualization/manager_plot.R")
+visualization_location <- file.path(lake_directory,"visualization")
+source(paste0(visualization_location,"/manager_plot.R"))
 
 #saved_file <- "/Users/quinn/Dropbox (VTFRS)/Research/SSC_forecasting/run_flare_package/wrr_runs_obs/wrr_runs_H_2019_03_14_2019_03_15_F_16_20201124T085027.nc"
 #qaqc_data_location <- "/Users/quinn/Dropbox/Research/SSC_forecasting/FLARE_package/flare_lake_examples/fcre/qaqc_data/"
